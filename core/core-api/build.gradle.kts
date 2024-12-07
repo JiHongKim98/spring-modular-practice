16e8925e25ff8cc4d5299ac0c53e2153a74bebb3
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-07 23:01:03 +0900

    feat: Add metric dependencies

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":core:core-domain"))
     implementation(project(":infra:storage"))
     runtimeOnly(project(":support:logging"))
+    runtimeOnly(project(":support:metric"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
